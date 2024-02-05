bed9ee1089e334909de7be19eb9c87d67ad0fd90
DimKra <dimkra@yandex.ru>
2024-02-05 09:43:36 +0300

    Update RadSensV4Blynk.tk.ino

--- RadSensV4Blynk.tk.ino
+++ RadSensV4Blynk.tk.ino
@@ -3,7 +3,7 @@
 #define BLYNK_PRINT Serial
 //#define BLYNK_TEMPLATE_ID "TMPL4mPNlWztI"
 //#define BLYNK_TEMPLATE_NAME "RadSens"
-#define BLYNK_AUTH_TOKEN "9V3d75MV9WoS96JqYYWoDxIMOql5IzKu"
+#define BLYNK_AUTH_TOKEN "-"
 #include <Wire.h>
 #include <CG_RadSens.h>
 #include <GyverOLED.h>
@@ -15,8 +15,8 @@
 #define OLED_NO_PRINT
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "Keenetic-3468";
-char pass[] = "Cwq4512%";
+char ssid[] = "---";
+char pass[] = "---";
 char auth[] = BLYNK_AUTH_TOKEN;
 //GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;        // с буфером
 //GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;     // без буфера
@@ -38,9 +38,9 @@ const int buttonPin = 12;
 boolean  buttonState = false;
 int light =1; //световая индикация вкл по умолчанию
 int sound = 0; //звуковая индикация выкл по умолчанию
-boolean press_flag = false; //признак нажатия кнопки  
+boolean press_flag = false; //признак нажатия кнопки
 boolean long_press_flag = false; //признак долгого нажатия кнопки
-unsigned long last_press = 0;   
+unsigned long last_press = 0;
 
 //Функция аудиоприветствия
 void hello() {
@@ -53,14 +53,14 @@ void hello() {
   oled.setScale(2);
   oled.setCursor(10, 3);
   oled.print("RadSens 5");
-  oled.update();  
+  oled.update();
   delay(3000);
-  oled.clear(); 
+  oled.clear();
 }
 
 //Функция, которая создаёт "трески" пьезоизлучателя при появлении импульсов
 // Функция, описывающая время и частоту пищания пьезоизлучателя
-void beep() {     
+void beep() {
   tone(buz_pin, 3500);
   delay(13);
   tone(buz_pin, 0);
@@ -73,7 +73,7 @@ void warning() {
     tone(buz_pin, 1500);
     delay(250);
     tone(buz_pin, 0);
-    delay(250);    
+    delay(250);
   }
 }
 
@@ -125,13 +125,13 @@ void rad_sign(int is) {
 
 void setup() {
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass, "blynk.tk", 8080); 
+  Blynk.begin(auth, ssid, pass, "blynk.tk", 8080);
   Wire.begin();
   oled.init(); // Инициализируем OLED в коде
-  oled.clear(); 
-  oled.update();  
+  oled.clear();
+  oled.update();
   pinMode(ADC_pin, OUTPUT); // Инициализируем АЦП как получатель данных
-  hello();  // Приветствуем пищанием  
+  hello();  // Приветствуем пищанием
   oled.update();  // Обновляем экран
   pulsesPrev = radSens.getNumberOfPulses(); // Записываем значение для предотвращения серии тресков на старте
   radSens.setSensitivity(580); //Установка чувствительности ссчетчика (105 для SBM20) первая проба 500
@@ -170,7 +170,7 @@ BLYNK_WRITE(V4) // this command is listening when something is written to V1
 void loop() {
   Blynk.run();
    // Раз в 250 мс происходит опрос счётчика импульсов для создания тресков, если число импульсов за 250 мс превысит 5, раздастся предупреждение
-  if (millis() - timer_imp > 250) {  
+  if (millis() - timer_imp > 250) {
     timer_imp = millis();
     int pulses = radSens.getNumberOfPulses();
     if (pulses - pulsesPrev > 5 ) {
@@ -189,7 +189,7 @@ void loop() {
     }
   }
   // Снимаем показания с дозиметра и выводим их на экран
-  if (millis() - timer_cnt > 2000) { 
+  if (millis() - timer_cnt > 2000) {
     timer_cnt = millis();
     pT=radSens.getNumberOfPulses();
     cpulses=(pT-pP)/2;
@@ -220,7 +220,7 @@ void loop() {
     }
     sprintf(buf2, "Ст: %.1fмкР/ч ", radSens.getRadIntensyStatic()); // Собираем строку с показаниями средней интенсивности за период работы
     sprintf(buf3, "%.1fcps ", cpulses);
-    
+
     //BLYNK_WRITE(V3);
     //BLYNK_WRITE(V4);
 
@@ -243,10 +243,10 @@ void loop() {
     oled.print(buf3);
   }
   // Считываем показание с АЦП, рисуем батарею и создаём индикацию заряда, показания АЦП вы можете подстроить под своё удобство
-  if (millis() - timer_bat > 5000) { 
+  if (millis() - timer_bat > 5000) {
     timer_bat = millis();
-    ADC = analogRead(ADC_pin); 
-    oled.rect(110, 0, 124, 9, OLED_STROKE); 
+    ADC = analogRead(ADC_pin);
+    oled.rect(110, 0, 124, 9, OLED_STROKE);
     oled.rect(125, 3, 126, 6, OLED_FILL);
     if (ADC >= 350) {
       oled.rect(112, 2, 114, 7, OLED_FILL);
@@ -265,7 +265,7 @@ void loop() {
       oled.rect(125, 3, 126, 6, OLED_FILL);
     }
   }
-  
+
   buttonState = digitalRead(buttonPin);
   if (buttonState == true  && press_flag == false && millis() - last_press > 100) {
     press_flag = !press_flag;
@@ -284,11 +284,11 @@ void loop() {
     }
   }
   if (buttonState == false && press_flag == true && long_press_flag == true) {
-    press_flag = !press_flag;            
-    long_press_flag = !long_press_flag;  
+    press_flag = !press_flag;
+    long_press_flag = !long_press_flag;
   }
    if (buttonState == false && press_flag == true && long_press_flag == false) {
-    press_flag = !press_flag;  
+    press_flag = !press_flag;
     Serial.println("короткое нажатие ");
     if (light==1) {
       light=0;
